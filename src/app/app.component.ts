37d6b410c15c5377c6b51d0186e5e283f6d49363
jannunzi <jannunzi@gmail.com>
2020-11-05 19:48:48 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'cs5610-f20-client-angular-jannunzi';
+}
